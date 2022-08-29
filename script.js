01a8af0920222d2e6acea903f66e3c4e5a08717e
Matheus <matheusdearaujo1209@gmail.com>
2022-08-28 23:04:43 -0300

    Versão Beta

---
+++ script.js
@@ -0,0 +1,28 @@
+const produto = document.getElementById("inputProduto")
+
+function adicionarProduto() {
+    const item = document.createElement("li")
+
+    item.innerHTML = `<li>
+        <span>${produto.value}</span>
+        <button class="excluir">X</button>
+    </li>`;
+
+    const lista = document.querySelector("#lista")
+    lista.appendChild(item)
+
+    produto.value = ""
+}
+
+const botaoAdd = document.querySelector("#bt-add")
+botaoAdd.addEventListener("click", adicionarProduto)
+
+function limparTudo(){
+    const lista = document.querySelector("#lista")
+    lista.innerHTML = ""
+}
+
+const botaoClear = document.querySelector(".botao2")
+
+botaoClear.addEventListener("click", limparTudo)
+
